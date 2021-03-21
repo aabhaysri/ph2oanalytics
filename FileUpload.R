1fed2eccad3b6a089e592e7d4f740515e4cc53d6
aabhaysri <80008507+aabhaysri@users.noreply.github.com>
2021-03-21 00:38:15 -0400

    Update FileUpload.R

--- FileUpload.R
+++ FileUpload.R
@@ -1,8 +1,13 @@
+#importing libraries
 library("tidyverse")
 library("googledrive")
 library("devtools")
+
+#making list of files
 files <- list.files(pattern=".*csv") 
 
+
+#for loop to upload the files to gdrive
 for(file in files){
   fileName <- paste0(file)
   drive_upload(file, path="~/Data Sets - pH2O Analytics/Datasets/Cleansed Data/", name=fileName)
